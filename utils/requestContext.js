7a67f4c5590609b3f7e84b74131d2c0558eb605f
Taimako Shamsudeen Abdul-Mumin Sugru <57481825+pyraxxz@users.noreply.github.com>
2026-09-03 02:24:55 +0000

    fix(auth): add request-local business authorization context

---
+++ utils/requestContext.js
@@ -0,0 +1,13 @@
+const { AsyncLocalStorage } = require('async_hooks');
+
+const requestContext = new AsyncLocalStorage();
+
+function runWithRequestContext(context, callback) {
+    return requestContext.run(context, callback);
+}
+
+function getRequestContext() {
+    return requestContext.getStore() || null;
+}
+
+module.exports = { getRequestContext, runWithRequestContext };
